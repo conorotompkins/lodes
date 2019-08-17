13c1931c83b79d5d715736a905ba7fd8e79fd39a
Conor <conorotompkins@gmail.com>
2019-08-17 16:02:55 -0400

    created separate scripts for networking with tracts

---
+++ scripts/load_data_tracts.R
@@ -0,0 +1,53 @@
+library(tidyverse)
+library(sf)
+library(tidycensus)
+library(tidygraph)
+library(ggraph)
+library(gganimate)
+
+Sys.getenv("CENSUS_API_KEY")
+options(tigris_use_cache = TRUE)
+
+theme_set(theme_bw())
+
+geo_crosswalk <- read_csv("data/pa_xwalk.csv.gz", col_types = cols(.default = "c"))
+
+allegheny_tracts <- get_decennial(geography = "tract",
+                           variables = c(total_pop = "P001001"),
+                           state = "PA",
+                           county = "Allegheny County",
+                           geometry = TRUE,
+                           output = "wide")
+
+allegheny_tracts %>% 
+  ggplot() +
+  geom_sf()
+
+df <- read_csv("data/pa_od_main_JT00_2015.csv.gz", col_types = cols(.default = "c"), 
+               #n_max = 500000
+) %>% 
+  mutate(S000 = as.numeric(S000),) %>% 
+  select(h_geocode, w_geocode, S000)
+
+df_tracts_summarized <- df %>% 
+  group_by(h_geocode, w_geocode) %>% 
+  summarize(jobs = sum(S000)) %>% 
+  ungroup() %>% 
+  arrange(desc(jobs))
+
+df_tracts_summarized <- df_tracts_summarized %>% 
+  left_join(geo_crosswalk %>% select(tabblk2010, trct), by = c("h_geocode" = "tabblk2010")) %>% 
+  rename(h_tract = trct) %>% 
+  left_join(geo_crosswalk %>% select(tabblk2010, trct), by = c("w_geocode" = "tabblk2010")) %>% 
+  rename(w_tract = trct)
+
+df_tracts_summarized <- df_tracts_summarized %>% 
+  group_by(h_tract, w_tract) %>% 
+  summarize(jobs = sum(jobs)) %>% 
+  ungroup()
+
+df_tracts_summarized <- df_tracts_summarized %>% 
+  semi_join(allegheny_tracts, by = c("h_tract" = "GEOID")) %>% 
+  semi_join(allegheny_tracts, by = c("w_tract" = "GEOID"))
+
+write_csv(df_tracts_summarized, "data/df_tracts_summarized.csv")
---
+++ scripts/networking_tracts.R
@@ -0,0 +1,129 @@
+library(tidyverse)
+library(sf)
+library(tidycensus)
+library(tidygraph)
+library(ggraph)
+
+Sys.getenv("CENSUS_API_KEY")
+options(tigris_use_cache = TRUE)
+
+df_intermediate <- read_csv("data/df_tracts_summarized.csv", col_types = cols(.default = "c")) %>% 
+  mutate(jobs = as.numeric(jobs)) %>% 
+  arrange(h_tract) %>% 
+  na.omit() %>% 
+  filter(!(h_tract == w_tract))
+
+allegheny_tracts <- get_decennial(geography = "tract",
+                           variables = c(total_pop = "P001001"),
+                           state = "PA",
+                           county = "Allegheny County",
+                           geometry = TRUE,
+                           output = "wide")
+
+#df_intermediate %>% 
+#  as_tbl_graph() %>% 
+#  activate(edges) %>% 
+#  filter(jobs > 100) %>% 
+#  ggraph(layout = "kk") +
+#  geom_node_point(size = .3) +
+#  geom_edge_fan(aes(edge_width = jobs, edge_alpha = jobs),
+#                arrow = arrow(length = unit(4, 'mm')), 
+#                start_cap = circle(3, 'mm'),
+#                end_cap = circle(3, 'mm')) +
+#  scale_edge_width_continuous(range = c(.3, 2)) +
+#  scale_edge_alpha_continuous(range = c(.1, 1))
+
+allegheny_tracts_centroids <- cbind(allegheny_tracts, st_coordinates(st_centroid(allegheny_tracts))) %>% 
+  st_set_geometry(NULL) %>% 
+  as_tibble() %>% 
+  rename(x = X,
+         y = Y) %>% 
+  select(GEOID, x, y)
+
+allegheny_tracts %>% 
+  ggplot() +
+  geom_sf() +
+  geom_point(data = allegheny_tracts_centroids, aes(x, y))
+  #geom_label(data = allegheny_tracts_centroids, aes(x, y, label = GEOID))
+
+allegheny_tracts_centroids <- allegheny_tracts_centroids %>% 
+  semi_join(df_intermediate, by = c("GEOID" = "h_tract")) %>% 
+  semi_join(df_intermediate, by = c("GEOID" = "w_tract"))
+
+df_intermediate <- df_intermediate %>% 
+  semi_join(allegheny_tracts_centroids, by = c("h_tract" = "GEOID")) %>% 
+  semi_join(allegheny_tracts_centroids, by = c("w_tract" = "GEOID"))
+
+g <- df_intermediate %>% 
+  as_tbl_graph(directed = TRUE)
+
+#node_pos <- allegheny_zcta_centroids
+
+#node_pos %>% 
+#  count(GEOID, sort = TRUE)
+
+minimum_jobs <- 200
+
+g <- g %>% 
+  activate(edges) %>% 
+  filter(jobs > minimum_jobs)
+
+graph_nodes <- g %>% 
+  activate(nodes) %>% 
+  as_tibble()
+
+graph_nodes
+
+g %>% 
+  activate(edges) %>% 
+  arrange(desc(jobs))
+
+node_pos <- allegheny_tracts_centroids
+
+layout <- create_layout(g, 'manual',
+                        node.positions = node_pos)
+
+manual_layout <- create_layout(graph = g,
+                               layout = "manual", node.positions = node_pos)
+
+legend_title <- str_c("Minimum: ", minimum_jobs, " commuters")
+legend_title
+
+ggraph(manual_layout) +
+  geom_sf(data = allegheny_tracts) +
+  #geom_node_label(aes(label = name),repel = FALSE) +
+  geom_node_point(alpha = 0) +
+  geom_edge_fan(aes(edge_width = jobs, edge_alpha = jobs),
+                arrow = arrow(length = unit(.5, 'lines')), 
+                start_cap = circle(.1, 'lines'),
+                end_cap = circle(.5, 'lines'),
+                color = "blue") +
+  scale_edge_width_continuous(legend_title, range = c(.1, 1.5)) +
+  scale_edge_alpha_continuous(legend_title, range = c(.1, 1), guide = "none") +
+  labs(x = NULL,
+       y = NULL,
+       title = "Where do people commute from/to for work?",
+       subtitle = "Based on 2015 US Census LODES dataset",
+       caption = "@conor_tompkins") +
+  theme_graph()
+
+
+##################################
+#to filter on nodes
+#tract_nodes <- g %>% 
+#  activate(nodes) %>% 
+#  as_tibble() %>% 
+#  mutate(index = row_number())
+
+#to_tract <- "42003020100"
+
+#to_tract_index <- tract_nodes %>% 
+#  filter(name == to_tract) %>% 
+#  select(index) %>% 
+#  pull()
+
+#g <- g %>% 
+#  activate(edges) %>% 
+#  filter(to != to_tract_index)
+#g
+
