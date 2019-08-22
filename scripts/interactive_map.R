e062c6eab8af82d921f1089da74bd1a382515826
Conor Tompkins <conorotompkins@gmail.com>
2019-08-22 13:01:49 +0000

    switched from zcta to tracts

--- scripts/interactive_map.R
+++ scripts/interactive_map.R
@@ -6,28 +6,11 @@ library(mapview)
 Sys.getenv("CENSUS_API_KEY")
 options(tigris_use_cache = TRUE)
 
-allegheny <- get_decennial(geography = "county",
-                           variables = c(total_pop = "P001001"),
-                           state = "PA",
-                           county = "Allegheny County",
-                           geometry = TRUE,
-                           output = "wide")
+allegheny_tracts <- get_decennial(geography = "tract",
+                                  variables = c(total_pop = "P001001"),
+                                  state = "PA",
+                                  county = "Allegheny County",
+                                  geometry = TRUE,
+                                  output = "wide")
 
-all_zips <- get_acs(geography = "zip code tabulation area",
-                    variables = c(total_pop = "B01003_001"),
-                    geometry = TRUE,
-                    output = "wide")
-
-allegheny_zcta <- st_intersection(allegheny, all_zips) %>% 
-  select(-c(GEOID, NAME)) %>% 
-  rename(GEOID = GEOID.1,
-         NAME = NAME.1)
-
-allegheny_zcta %>% 
-  ggplot() +
-  geom_sf(aes(fill = total_popE)) +
-  scale_fill_viridis_c()
-
-allegheny_zcta
-
-mapview(allegheny_zcta)
+mapview(allegheny_tracts)
