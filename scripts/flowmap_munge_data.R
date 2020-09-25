63f2af214bc78aa7ede887773203b085cf7e0c1c
Conor Tompkins <conorotompkins@gmail.com>
2020-09-25 10:53:45 -0400

    replaced individual calls to LODES with vectorized version

--- scripts/flowmap_munge_data.R
+++ scripts/flowmap_munge_data.R
@@ -15,249 +15,32 @@ options(tigris_use_cache = TRUE,
         digits = 4)
 
 #get lodes
-##pa
-lodes_od_pa_main <- grab_lodes(state = "pa", year = 2017, 
-                               lodes_type = "od", job_type = "JT00", 
-                               segment = "S000", state_part = "main", 
-                               agg_geo = "county") %>% 
+states <- c("pa", "oh", "wv", "va", "dc", "de",
+            "md", "ny", "ri", "ct", "ma", "vt", "nh", "me")
+
+lodes_od_main <- grab_lodes(state = states, year = 2017, 
+                            lodes_type = "od", job_type = "JT00", 
+                            segment = "S000", state_part = "main", 
+                            agg_geo = "county") %>% 
   select(state, w_county, h_county, S000, year) %>% 
   rename(commuters = S000)
 
-lodes_od_pa_aux <- grab_lodes(state = "pa", year = 2017, 
-                              lodes_type = "od", job_type = "JT00", 
-                              segment = "S000", state_part = "aux", 
-                              agg_geo = "county") %>% 
+lodes_od_aux <- grab_lodes(state = states, year = 2017, 
+                           lodes_type = "od", job_type = "JT00", 
+                           segment = "S000", state_part = "aux", 
+                           agg_geo = "county") %>% 
   select(state, w_county, h_county, S000, year) %>% 
   rename(commuters = S000)
 
-##oh
-lodes_od_oh_main <- grab_lodes(state = "oh", year = 2017, 
-                               lodes_type = "od", job_type = "JT00", 
-                               segment = "S000", state_part = "main", 
-                               agg_geo = "county") %>% 
-  select(state, w_county, h_county, S000, year) %>% 
-  rename(commuters = S000)
-
-lodes_od_oh_aux <- grab_lodes(state = "oh", year = 2017, 
-                              lodes_type = "od", job_type = "JT00", 
-                              segment = "S000", state_part = "aux", 
-                              agg_geo = "county") %>% 
-  select(state, w_county, h_county, S000, year) %>% 
-  rename(commuters = S000)
-
-##ny
-lodes_od_ny_main <- grab_lodes(state = "ny", year = 2017, 
-                               lodes_type = "od", job_type = "JT00", 
-                               segment = "S000", state_part = "main", 
-                               agg_geo = "county") %>% 
-  select(state, w_county, h_county, S000, year) %>% 
-  rename(commuters = S000)
-
-lodes_od_ny_aux <- grab_lodes(state = "ny", year = 2017, 
-                              lodes_type = "od", job_type = "JT00", 
-                              segment = "S000", state_part = "aux", 
-                              agg_geo = "county") %>% 
-  select(state, w_county, h_county, S000, year) %>% 
-  rename(commuters = S000)
-
-##md
-lodes_od_md_main <- grab_lodes(state = "md", year = 2017, 
-                               lodes_type = "od", job_type = "JT00", 
-                               segment = "S000", state_part = "main", 
-                               agg_geo = "county") %>% 
-  select(state, w_county, h_county, S000, year) %>% 
-  rename(commuters = S000)
-
-lodes_od_md_aux <- grab_lodes(state = "md", year = 2017, 
-                              lodes_type = "od", job_type = "JT00", 
-                              segment = "S000", state_part = "aux", 
-                              agg_geo = "county") %>% 
-  select(state, w_county, h_county, S000, year) %>% 
-  rename(commuters = S000)
-
-##nj
-lodes_od_nj_main <- grab_lodes(state = "nj", year = 2017, 
-                               lodes_type = "od", job_type = "JT00", 
-                               segment = "S000", state_part = "main", 
-                               agg_geo = "county") %>% 
-  select(state, w_county, h_county, S000, year) %>% 
-  rename(commuters = S000)
-
-lodes_od_nj_aux <- grab_lodes(state = "nj", year = 2017, 
-                              lodes_type = "od", job_type = "JT00", 
-                              segment = "S000", state_part = "aux", 
-                              agg_geo = "county") %>% 
-  select(state, w_county, h_county, S000, year) %>% 
-  rename(commuters = S000)
-
-##wv
-lodes_od_wv_main <- grab_lodes(state = "wv", year = 2017, 
-                               lodes_type = "od", job_type = "JT00", 
-                               segment = "S000", state_part = "main", 
-                               agg_geo = "county") %>% 
-  select(state, w_county, h_county, S000, year) %>% 
-  rename(commuters = S000)
-
-lodes_od_wv_aux <- grab_lodes(state = "wv", year = 2017, 
-                              lodes_type = "od", job_type = "JT00", 
-                              segment = "S000", state_part = "aux", 
-                              agg_geo = "county") %>% 
-  select(state, w_county, h_county, S000, year) %>% 
-  rename(commuters = S000)
-
-##de
-lodes_od_de_main <- grab_lodes(state = "de", year = 2017, 
-                               lodes_type = "od", job_type = "JT00", 
-                               segment = "S000", state_part = "main", 
-                               agg_geo = "county") %>% 
-  select(state, w_county, h_county, S000, year) %>% 
-  rename(commuters = S000)
-
-lodes_od_de_aux <- grab_lodes(state = "de", year = 2017, 
-                              lodes_type = "od", job_type = "JT00", 
-                              segment = "S000", state_part = "aux", 
-                              agg_geo = "county") %>% 
-  select(state, w_county, h_county, S000, year) %>% 
-  rename(commuters = S000)
-
-##va
-lodes_od_va_main <- grab_lodes(state = "va", year = 2017, 
-                               lodes_type = "od", job_type = "JT00", 
-                               segment = "S000", state_part = "main", 
-                               agg_geo = "county") %>% 
-  select(state, w_county, h_county, S000, year) %>% 
-  rename(commuters = S000)
-
-lodes_od_va_aux <- grab_lodes(state = "va", year = 2017, 
-                              lodes_type = "od", job_type = "JT00", 
-                              segment = "S000", state_part = "aux", 
-                              agg_geo = "county") %>% 
-  select(state, w_county, h_county, S000, year) %>% 
-  rename(commuters = S000)
-
-##ct
-lodes_od_ct_main <- grab_lodes(state = "ct", year = 2017, 
-                               lodes_type = "od", job_type = "JT00", 
-                               segment = "S000", state_part = "main", 
-                               agg_geo = "county") %>% 
-  select(state, w_county, h_county, S000, year) %>% 
-  rename(commuters = S000)
-
-lodes_od_ct_aux <- grab_lodes(state = "ct", year = 2017, 
-                              lodes_type = "od", job_type = "JT00", 
-                              segment = "S000", state_part = "aux", 
-                              agg_geo = "county") %>% 
-  select(state, w_county, h_county, S000, year) %>% 
-  rename(commuters = S000)
-
-##ri
-lodes_od_ri_main <- grab_lodes(state = "ri", year = 2017, 
-                               lodes_type = "od", job_type = "JT00", 
-                               segment = "S000", state_part = "main", 
-                               agg_geo = "county") %>% 
-  select(state, w_county, h_county, S000, year) %>% 
-  rename(commuters = S000)
-
-lodes_od_ri_aux <- grab_lodes(state = "ri", year = 2017, 
-                              lodes_type = "od", job_type = "JT00", 
-                              segment = "S000", state_part = "aux", 
-                              agg_geo = "county") %>% 
-  select(state, w_county, h_county, S000, year) %>% 
-  rename(commuters = S000)
-
-##ma
-lodes_od_ma_main <- grab_lodes(state = "ma", year = 2017, 
-                               lodes_type = "od", job_type = "JT00", 
-                               segment = "S000", state_part = "main", 
-                               agg_geo = "county") %>% 
-  select(state, w_county, h_county, S000, year) %>% 
-  rename(commuters = S000)
-
-lodes_od_ma_aux <- grab_lodes(state = "ma", year = 2017, 
-                              lodes_type = "od", job_type = "JT00", 
-                              segment = "S000", state_part = "aux", 
-                              agg_geo = "county") %>% 
-  select(state, w_county, h_county, S000, year) %>% 
-  rename(commuters = S000)
-
-##vt
-lodes_od_vt_main <- grab_lodes(state = "vt", year = 2017, 
-                               lodes_type = "od", job_type = "JT00", 
-                               segment = "S000", state_part = "main", 
-                               agg_geo = "county") %>% 
-  select(state, w_county, h_county, S000, year) %>% 
-  rename(commuters = S000)
-
-lodes_od_vt_aux <- grab_lodes(state = "vt", year = 2017, 
-                              lodes_type = "od", job_type = "JT00", 
-                              segment = "S000", state_part = "aux", 
-                              agg_geo = "county") %>% 
-  select(state, w_county, h_county, S000, year) %>% 
-  rename(commuters = S000)
-
-##nh
-lodes_od_nh_main <- grab_lodes(state = "nh", year = 2017, 
-                               lodes_type = "od", job_type = "JT00", 
-                               segment = "S000", state_part = "main", 
-                               agg_geo = "county") %>% 
-  select(state, w_county, h_county, S000, year) %>% 
-  rename(commuters = S000)
-
-lodes_od_nh_aux <- grab_lodes(state = "nh", year = 2017, 
-                              lodes_type = "od", job_type = "JT00", 
-                              segment = "S000", state_part = "aux", 
-                              agg_geo = "county") %>% 
-  select(state, w_county, h_county, S000, year) %>% 
-  rename(commuters = S000)
-
-##me
-lodes_od_me_main <- grab_lodes(state = "me", year = 2017, 
-                               lodes_type = "od", job_type = "JT00", 
-                               segment = "S000", state_part = "main", 
-                               agg_geo = "county") %>% 
-  select(state, w_county, h_county, S000, year) %>% 
-  rename(commuters = S000)
-
-lodes_od_me_aux <- grab_lodes(state = "me", year = 2017, 
-                              lodes_type = "od", job_type = "JT00", 
-                              segment = "S000", state_part = "aux", 
-                              agg_geo = "county") %>% 
-  select(state, w_county, h_county, S000, year) %>% 
-  rename(commuters = S000)
-
-##dc
-lodes_od_dc_main <- grab_lodes(state = "dc", year = 2017, 
-                               lodes_type = "od", job_type = "JT00", 
-                               segment = "S000", state_part = "main", 
-                               agg_geo = "county") %>% 
-  select(state, w_county, h_county, S000, year) %>% 
-  rename(commuters = S000)
-
-lodes_od_dc_aux <- grab_lodes(state = "dc", year = 2017, 
-                              lodes_type = "od", job_type = "JT00", 
-                              segment = "S000", state_part = "aux", 
-                              agg_geo = "county") %>% 
-  select(state, w_county, h_county, S000, year) %>% 
-  rename(commuters = S000)
-
-lodes_combined <- bind_rows(lodes_od_pa_main, lodes_od_pa_aux,
-                            lodes_od_ny_main, lodes_od_ny_aux,
-                            lodes_od_ct_main, lodes_od_ct_aux,
-                            lodes_od_ri_main, lodes_od_ri_aux,
-                            lodes_od_ma_main, lodes_od_ma_aux,
-                            lodes_od_vt_main, lodes_od_vt_aux,
-                            lodes_od_nh_main, lodes_od_nh_aux,
-                            lodes_od_me_main, lodes_od_me_aux,
-                            lodes_od_md_main, lodes_od_md_aux,
-                            lodes_od_nj_main, lodes_od_nj_aux,
-                            lodes_od_wv_main, lodes_od_wv_aux,
-                            lodes_od_de_main, lodes_od_de_aux,
-                            lodes_od_va_main, lodes_od_va_aux,
-                            lodes_od_dc_main, lodes_od_dc_aux)
+lodes_combined <- bind_rows(lodes_od_main, lodes_od_aux)
 
 lodes_combined %>% 
   count(h_county, w_county, sort = TRUE) %>% 
   filter(n > 1)
+
+lodes_combined %>% 
+  count(h_county, w_county, sort = TRUE) %>% 
+  distinct(n)
 
 #get geometry
 counties_combined <- tigris::counties(state = c("PA", "NY", "NJ", "MD", 
