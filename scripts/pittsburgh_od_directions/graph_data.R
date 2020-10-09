ad323d248439d61f2b4508c7a78294362f2a9aa6
Conor Tompkins <conorotompkins@gmail.com>
2020-10-09 17:16:15 -0400

    pointed script to combined processed tract file, added graphs to compare h_tract and w_tract durations

--- scripts/pittsburgh_od_directions/graph_data.R
+++ scripts/pittsburgh_od_directions/graph_data.R
@@ -17,13 +17,18 @@ allegheny_county_tracts <- tracts(state = "PA", county = "Allegheny", cb = TRUE)
   select(GEOID)
 
 #read in shp file
-tract_od_directions <- st_read("data/tract_od_total_shape/tract_od_total_shape.shp") %>% 
+st_read("data/tract_od_total_shape_combined/tract_od_total_shape_combined.shp") %>% 
+  names()
+
+tract_od_directions <- st_read("data/tract_od_total_shape_combined/tract_od_total_shape_combined.shp") %>% 
   rename(home_address = hm_ddrs,
          work_address = wrk_ddr,
          distance = distanc,
          duration = duratin,
-         instruction = instrct,
-         commuters = commtrs)
+         directions = dirctns,
+         instructions = instrct,
+         commuters = commtrs,
+         source_file = sorc_fl)
 
 glimpse(tract_od_directions)
 
@@ -31,7 +36,27 @@ tract_od_directions %>%
  filter(is.na(geometry))
 
 #read in od stats
-tract_od_stats <- read_csv("data/tract_od_stats.csv")
+# tract_od_stats <- read_csv("data/tract_od_stats.csv",
+#                            col_types = cols(
+#                              h_tract = col_character(),
+#                              home_address = col_character(),
+#                              w_tract = col_character(),
+#                              work_address = col_character(),
+#                              duration = col_double(),
+#                              distance = col_double(),
+#                              steps = col_double(),
+#                              commuters = col_double(),
+#                              geometry = col_character()
+#                            ))
+
+tract_od_stats <- tract_od_directions %>% 
+  unnest(directions) %>%
+  group_by(h_tract, home_address, w_tract, work_address) %>%
+  summarize(duration = sum(duration),
+            distance = sum(distance),
+            steps = n(),
+            commuters = unique(commuters)) %>% 
+  ungroup()
 
 #graph od stats
 tract_od_stats %>% 
@@ -50,6 +75,7 @@ tract_od_stats %>%
 tract_od_directions %>% 
   group_by(h_tract, home_address, w_tract, work_address) %>% 
   summarize() %>% 
+  ungroup() %>% 
   count(h_tract, w_tract, sort = TRUE) %>% 
   filter(n > 1)
 
@@ -86,7 +112,8 @@ tract_od_summarized %>%
   st_as_sf() %>% 
   ggplot() +
   geom_sf(aes(fill = mean_duration), size = .2) +
-  scale_fill_viridis_c() +
+  scale_fill_viridis_c(na.value = "grey90") +
+  labs(title = "Origin") +
   theme_void()
 
 tract_od_summarized %>% 
@@ -98,5 +125,28 @@ tract_od_summarized %>%
   st_as_sf() %>% 
   ggplot() +
   geom_sf(aes(fill = mean_duration), size = .2) +
-  scale_fill_viridis_c() +
+  scale_fill_viridis_c(na.value = "grey90") +
+  labs(title = "Destination") +
   theme_void()
+
+tract_od_summarized %>% 
+  select(h_tract, w_tract, duration) %>% 
+  pivot_longer(contains("tract")) %>%
+  group_by(name, value) %>% 
+  summarize(avg_duration = mean(duration)) %>% 
+  arrange(value) %>% 
+  pivot_wider(names_from = name, values_from = avg_duration, values_fill = 0) %>% 
+  ggplot(aes(h_tract, w_tract)) +
+  geom_point(alpha = .7)
+
+
+tract_od_summarized %>% 
+  select(h_tract, w_tract, duration, commuters) %>%
+  mutate(h_tract = fct_reorder(h_tract, commuters),
+         w_tract = fct_reorder(w_tract, commuters)) %>% 
+  complete(h_tract, w_tract, fill = list(duration = NA)) %>% 
+  ggplot(aes(h_tract, w_tract, fill = duration)) +
+  geom_tile() +
+  scale_fill_viridis_c(na.value = "grey90") +
+  theme(panel.grid = element_blank())
+  
